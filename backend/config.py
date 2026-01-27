f46b8a7070c9d07d70ea3b044310b3d3962a5a77
saraadeshmukh13 <saradeshmukh007@gmail.com>
2026-01-27 10:55:18 +0530

    Add base configuration for Flask, database, and JWT setup

--- backend/config.py
+++ backend/config.py
@@ -4,3 +4,16 @@ from dotenv import load_dotenv
 
 #load variables from .env file (only once , globally)
 load_dotenv()
+
+class BaseConfig:
+#core flask configuration
+SECRET_KEY = os.getenv("SECRET_KEY","eco-learn-secret-key")
+
+#database configuration
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///ecolearn.db"
+)
+SQLALCHEMY_TRACK_MODIFICATION = False
+
+#JWT configuration
