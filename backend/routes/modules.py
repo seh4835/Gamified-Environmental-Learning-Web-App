c6803808cf4e1a553c0a8c6829133d96834ef5ba
seh4835 <sehersanghani5@gmail.com>
2026-02-09 12:12:40 +0530

    fixed indentation

--- backend/routes/modules.py
+++ backend/routes/modules.py
@@ -24,34 +24,38 @@ def get_all_modules():
             "points":module.points
         })
         return jsonify(result), 200
-    #get single modules + quiz questions
-    @modules_bp.route("/<int:module_id>",methods=["GET"])
-    @jwt_required()
-    def get_module_detail(module_id):
-        module = LearningModule.query.get(module_id)
-        if not module:
-            return jsonify({"error":"Module not found"}), 404
-        quizzes = QuizQuestion.query.filter_by(module_id=module.id.all()
-                                               quiz_data =[]
-                                               for quiz in quizzes:
-                                                quiz_data.append({
-                                                    "id":quiz.id,
-                                                    "question":quiz.question,
-                                                    "option_a":quiz.option_a,
-                                                    "option_b":quiz.option_b,
-                                                    "option_c":quiz.option_c,
-                                                    "option_d":quiz.option_d,
-                                                    #correct_option intentionally NOT sent
-                                                }))
-        response = {
-            "id":module.id,
-            "title":module.title,
-            "description":module.description,
-            "difficulty":module.difficulty,
-            "points":module.points,
-            "quizzes":quiz_data
-        }
+    
+#get single modules + quiz questions
+@modules_bp.route("/<int:module_id>",methods=["GET"])
+@jwt_required()
+def get_module_detail(module_id):
+    module = LearningModule.query.get(module_id)
+    if not module:
+        return jsonify({"error":"Module not found"}), 404
+    
+    quizzes = QuizQuestion.query.filter_by(module_id=module.id).all()
 
-        return jsonify(response),200
+    quiz_data = []
+    for quiz in quizzes:
+        quiz_data.append({
+            "id": quiz.id,
+            "question": quiz.question,
+            "option_a": quiz.option_a,
+            "option_b": quiz.option_b,
+            "option_c": quiz.option_c,
+            "option_d": quiz.option_d
+            # correct_option intentionally NOT sent
+        })
+
+    response = {
+        "id": module.id,
+        "title": module.title,
+        "description": module.description,
+        "difficulty": module.difficulty,
+        "points": module.points,
+        "quizzes": quiz_data
+    }
+
+    return jsonify(response), 200
         
         
