01ee8b48e031cd271c4939a69d6f5ee019546c70
saraadeshmukh13 <saradeshmukh007@gmail.com>
2026-02-09 12:33:16 +0530

    add user dashboard summary endpoint

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,3 +1,5 @@
+from flask import Blueprint , jsonify 
+from flask_jwt_extended import jwt_required, get_jwt_identity
 #BluePrint Initialization
 users_bp = Blueprint("users",__name__)
 
@@ -21,3 +23,30 @@ def get_my_profile():
         "created_at":user.created_at.isoformat()
 
     }), 200
+
+#get user dashboard summary
+
+@users_bp.route("/dashboard",methods=["GET"])
+@jwt_required()
+
+def get_dashboard_summary():
+
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    
+    badges = (
+        db.session.query(Badge)
+        .join(UserBagde, UserBadge.badge_id == Badge.id)
+        .filter(UserBadge.user_id == user.id)
+        .all()
+    )
+
+    quiz_attempts_count = QuizAttempt.query.filter_by(user_id=user.id).count()
+
+    challenges_completed_count = ChallengeSubmission.query.filter_by(
+        user_id=user.id,
+        status="approved"
+    ).count()
