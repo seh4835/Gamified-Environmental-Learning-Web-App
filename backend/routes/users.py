83559873bc42ec1fbef3c5b67ca6557e99a1f212
saraadeshmukh13 <saradeshmukh007@gmail.com>
2026-02-09 12:08:08 +0530

    add get_my_profile function for user profile

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -4,3 +4,20 @@ users_bp = Blueprint("users",__name__)
 #get current user profile
 @users_bp.route("/me",methods=["GET"])
 @jwt_required()
+def get_my_profile():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({"error":"User not found"}), 404
+    
+    return jsonify({
+        "id":user.id,
+        "name":user.name,
+        "email":user.email,
+        "role":user.role,
+        "instituion":user.instituion,
+        "eco_points":user.eco_points,
+        "created_at":user.created_at.isoformat()
+
+    }), 200
