deb6d4608ea7ffc9764b22ecc821337d98797547
seh4835 <sehersanghani5@gmail.com>
2026-04-13 12:48:33 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,7 +7,7 @@ import axios from "axios";
 */
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://gamified-environmental-learning-web-app.onrender.com",
   headers: {
     "Content-Type": "application/json"
   }
